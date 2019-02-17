a5bc7ee25557a171ce034d55b59de872b93f7abc
jbyrneie <jbyrne@glgroup.com>
2019-02-17 17:48:11 +0000

    Configure Players modal

--- components/appBar.js
+++ components/appBar.js
@@ -3,11 +3,14 @@ import { Platform, StyleSheet, Text, View, TouchableWithoutFeedback } from 'reac
 import Menu, { MenuItem, MenuDivider } from 'react-native-material-menu';
 import { GlobalStyles } from '../src/styles';
 import Icon from 'react-native-fa-icons';
-import {getContactDetails, saveContactDetails} from '../src/utils'
+import ConfigPlayers from './configPlayers'
 
 class AppBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showConfigPlayersModal: false
+    };
   }
   _menu = null;
 
@@ -43,34 +46,51 @@ class AppBar extends Component {
 
   _menuPlayers() {
     console.log('_menuPlayers');
+    this.setState({
+      showConfigPlayersModal: true
+    })
     this._menu.hide();
+  }
+
+  _showConfigPlayersModal(showModal) {
+    this.setState({
+      showConfigPlayersModal: showModal
+    })
   }
 
   render() {
     return (
-      <View style={styles.header}>
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <View style={{flex:.75}}>
-            <Text style={styles.page_title}>
-              {Platform.OS === 'ios'?<Icon name='chevron-left' style={styles.ios_icon}/>:null}
-              {this.props.bannerText}
-            </Text>
+      <View>
+        <View style={styles.header}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <View style={{flex:.75}}>
+              <Text style={styles.page_title}>
+                {Platform.OS === 'ios'?<Icon name='chevron-left' style={styles.ios_icon}/>:null}
+                {this.props.bannerText}
+              </Text>
+            </View>
+            <View style={{flex:.25, alignItems: 'center', justifyContent: 'center'}}>
+              <TouchableWithoutFeedback onPress={this.showMenu}>
+                <View>
+                  <Icon name='align-justify' style={styles.ios_icon}/>
+                </View>
+              </TouchableWithoutFeedback>
+              <Menu
+                ref={this.setMenuRef}
+                button={<Text></Text>}
+              >
+                <MenuItem onPress={this._menuPlayers.bind(this)}>Players</MenuItem>
+                <MenuItem onPress={this._menuLogout.bind(this, this.props.navigator, this.props.url)}>Logout</MenuItem>
+              </Menu>
+            </View>
           </View>
-          <View style={{flex:.25, alignItems: 'center', justifyContent: 'center'}}>
-            <TouchableWithoutFeedback onPress={this.showMenu}>
-              <View>
-                <Icon name='align-justify' style={styles.ios_icon}/>
-              </View>
-            </TouchableWithoutFeedback>
-            <Menu
-              ref={this.setMenuRef}
-              button={<Text></Text>}
-            >
-              <MenuItem onPress={this._menuPlayers.bind(this)}>Players</MenuItem>
-              <MenuDivider />
-              <MenuItem onPress={this._menuLogout.bind(this, this.props.navigator, this.props.url)}>Logout</MenuItem>
-            </Menu>
-          </View>
+        </View>
+        <View>
+          <ConfigPlayers
+            visible={this.state.showConfigPlayersModal}
+            showModal={this._showConfigPlayersModal.bind(this)}
+            navigator={this.props.navigator}
+          />
         </View>
       </View>
     );

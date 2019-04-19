fc235508e7a728260aa4c1acb1c00d97c7616ae6
jbyrneie <jbyrne@glgroup.com>
2019-04-19 17:39:36 +0100

    About info

---
+++ README.md
@@ -0,0 +1,14 @@
+## R2D2
+
+A golf tee-time booking Mobile App for iOS and Android built using react-native
+
+### PreReqs
+- Install Android Studio
+- Configure an Emulator and start it from Android Studio
+
+### Installation
+
+```
+npm install
+react-native run-android
+````
--- components/about.js
+++ components/about.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, View } from 'react-native';
 import { GlobalStyles } from '../src/styles';
 import AppBar from './appBar'
 
@@ -8,17 +8,33 @@ class About extends Component {
     return (
       <View style={styles.container}>
         <AppBar bannerText='About' navigation={this.props.navigation}/>
-        <View style={styles.body}>
+        <ScrollView style={styles.body}>
           <Text style={[styles.heading, styles.text]}>
             About R2D2
           </Text>
           <Text style={styles.text}>
-            This App lets you.....
+            R2D2 enables you to beat the queue and reserve a tee-time on your Local Golf Club that is registered on the BRS
+            system.
           </Text>
           <Text style={styles.text}>
-            Its also lets you.....
+            To get started, provide your BRS Login ID and password. R2D2 will authenticate you against the BRS system
+            to ensure your credentials are correct.
           </Text>
-        </View>
+          <Text style={styles.text}>
+            The next task is to setup the Players you want to reserve a tee-time for. Simply give each player a name and
+            provide the BRS ID for that player. This is a once off task, unless you wish to change your "friends" and reserve
+            a tee-time for other "friends".
+          </Text>
+          <Text style={styles.text}>
+            Finally, enter when the timesheet becomes available, select the tee-time slot you require, add yourself and up to 3 "friends"
+            to the list. R2D2 will then "wake-up" when the timesheet is live and you and your friends will be the first to
+            get that coveted tee-time. If for some reason, the tee-time is not available, R2D2 will look at the next slot for
+            availability. The BRS system will send you an email confirmation of your tee-time as normal.
+          </Text>
+          <Text style={styles.text}>
+            Enjoy your round!
+          </Text>
+        </ScrollView>
       </View>
     );
   }
